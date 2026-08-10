da18769f89e0d2e6612e8e5a3e32e0ab090013c7
Ankit-Roy-CSE <ankit2003.ar15@gmail.com>
2026-08-10 06:18:07 +0000

    feat: initialize Express server with MongoDB connection and update environment configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import passport from "passport";
+import { Env } from "./config/env.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import connectDatabase from "./config/database.config";
+
+const app = express();
+const server = http.createServer(app);
+
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: Env.FRONTEND_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.get(
+    "/health",
+    asyncHandler(async (req: Request, res: Response) => {
+        res.status(HTTPSTATUS.OK).json({
+            message: "Server is healthy",
+            status: "OK",
+        });
+    })
+);
+
+app.use(errorHandler);
+
+server.listen(Env.PORT, async () => {
+    await connectDatabase();
+    console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
+});
